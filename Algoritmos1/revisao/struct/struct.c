4ea1e7f73be10af320c5a0e3bc1cf680978d0e4b
Gabriel Prando <gprando55@gmail.com>
2020-02-29 16:50:28 -0300

    agora deu certo esse crl

--- Algoritmos1/revisao/struct/struct.c
+++ Algoritmos1/revisao/struct/struct.c
@@ -10,27 +10,31 @@ typedef struct
   float peso;
 } Aluno;
 
-// void alteraNome(Aluno *a1)
-// {
-//   //a1->nome[MAXTAM]="jubileu";
-//   // Aluno *aux = malloc(sizeof(Aluno));
+void alteraNome(Aluno *a1)
+{
+  Aluno *aux = malloc(sizeof(Aluno));
   
-//   // strcpy(aux->nome, "jubileu");
-//   // strcpy(a1->nome,aux->nome);
-// }
+  strcpy(aux->nome, "jubileu");
+  strcpy(a1->nome,aux->nome);
+  free(aux);
+}
 
 int main(void)
-{
-  Aluno *a1;
+{ 
+  /*
+  Aluno *a1 = malloc(Passar a quantidade de bits necessários para alocar);
+  sizeof retorna quantos bits a parada tem
+  */
+  Aluno *a1 = malloc(sizeof(Aluno));
   strcpy(a1->nome, "joao");
   a1->ra = 123;
   a1->peso = 71.5;
 
   printf("o nome do aluno é %s \n", a1->nome);
   printf("o peso do aluno é %f \n", a1->peso);
-  // //alteraNome(a1);
-  // printf("Nome alterado para :");
-  // printf("%s \n", a1->nome);
+  alteraNome(a1);
+  printf("Nome alterado para :");
+  printf("%s \n", a1->nome);
 
   printf("\n");
   return 0;
