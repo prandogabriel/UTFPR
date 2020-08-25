27578ca275a038452b0a3f0d7ba2844c5df0c616
Prando <gprando55@gmail.com>
2020-08-25 14:42:39 -0300

    fix: identaçao :smile:

--- Algoritmos2/divisao_conquita/potencia.c
+++ Algoritmos2/divisao_conquita/potencia.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int potenciaInterativa(int a, int n){
-  
+
     int i, aux;
 
     if (n <= 0)
@@ -19,19 +19,19 @@ int potenciaInterativa(int a, int n){
 }
 
 int potenciaDivisaoConquista(int a, int n){
-    if (n <= 0)
-        return 1;
-    else if (n == 1)
-        return a;
+  if (n <= 0)
+    return 1;
+  else if (n == 1)
+    return a;
+  else
+    if (n % 2 == 0)
+      return potenciaDivisaoConquista(a, n / 2) * potenciaDivisaoConquista(a, n / 2);
     else
-        if (n % 2 == 0)
-            return potenciaDivisaoConquista(a, n / 2) * potenciaDivisaoConquista(a, n / 2);
-        else
-            return a * potenciaDivisaoConquista(a, (n - 1) / 2) * potenciaDivisaoConquista(a, (n - 1) / 2);
+      return a * potenciaDivisaoConquista(a, (n - 1) / 2) * potenciaDivisaoConquista(a, (n - 1) / 2);
 }
 
-int main(){
-    printf("%d elevado a %d = %d\n", 2, 8, potenciaInterativa(2, 8));
-    printf("%d elevado a %d = %d\n", 2, 8, potenciaDivisaoConquista(2, 8));
-    return 0;
+int main() {
+  printf("%d elevado a %d = %d\n", 2, 8, potenciaInterativa(2, 8));
+  printf("%d elevado a %d = %d\n", 2, 8, potenciaDivisaoConquista(2, 8));
+  return 0;
 }
