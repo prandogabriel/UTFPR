e6ff9283a9a6c317bc769919cf63a929436eb35e
gprando <gprando55@gmail.com>
2021-08-17 23:58:40 -0300

    chore: add final version test :construction:

--- banco-de-dados-2/prova-1/index.sql
+++ banco-de-dados-2/prova-1/index.sql
@@ -1,28 +1,34 @@
-CREATE TABLE Aluno(
-  Nome VARCHAR(50) NOT NULL,
-  RA DECIMAL(8) NOT NULL,
-  DataNasc DATE NOT NULL,
+-- Gabriel Prando, RA: 2039869
+-- Prova 1 Banco de dados 2, semestre 2021.1
+-- Professor Ives Pola
+-- código disponível também em -> https://github.com/gprando/UTFPR/tree/master/banco-de-dados-2/prova-1
+-- Para o desenvolvimento, foi utilizado docker para rodar o banco e postgres na versão  13.4 (LTS) sem nenhuma configuração extra do banco
+
+create table Aluno(
+  Nome VARCHAR(50) not null,
+  RA DECIMAL(8) not null,
+  DataNasc DATE not null,
   Idade DECIMAL(3),
-  NomeMae VARCHAR(50) NOT NULL,
+  NomeMae VARCHAR(50) not null,
   Cidade VARCHAR(30),
   Estado CHAR(2),
   Curso VARCHAR(50),
   periodo integer
 );
-CREATE TABLE Discip(
-  Sigla CHAR(7) NOT NULL,
-  Nome VARCHAR(25) NOT NULL,
+create table Discip(
+  Sigla CHAR(7) not null,
+  Nome VARCHAR(25) not null,
   SiglaPreReq CHAR(7),
-  NNCred DECIMAL(2) NOT NULL,
+  NNCred DECIMAL(2) not null,
   Monitor DECIMAL(8),
   Depto CHAR(8)
 );
-CREATE TABLE Matricula(
-  RA DECIMAL(8) NOT NULL,
-  Sigla CHAR(7) NOT NULL,
-  Ano CHAR(4) NOT NULL,
-  Semestre CHAR(1) NOT NULL,
-  CodTurma DECIMAL(4) NOT NULL,
+create table Matricula(
+  RA DECIMAL(8) not null,
+  Sigla CHAR(7) not null,
+  Ano CHAR(4) not null,
+  Semestre CHAR(1) not null,
+  CodTurma DECIMAL(4) not null,
   NotaP1 DECIMAL(3),
   NotaP2 DECIMAL(3),
   NotaTrab DECIMAL(3),
@@ -33,17 +39,17 @@ CREATE TABLE Matricula(
 -- table: Matricula PK_Matricula; Type: CONSTRAINT; Owner: postgres
 --
 ALTER TABLE ONLY Matricula
-ADD CONSTRAINT "PK_Matricula" PRIMARY KEY (RA, Sigla);
+add constraint "PK_Matricula" primary key (RA, Sigla);
 --
 -- table: Discip PK_Discip; Type: CONSTRAINT; Owner: postgres
 --
 ALTER TABLE ONLY Discip
-ADD CONSTRAINT "PK_Discip" PRIMARY KEY (Sigla);
+add constraint "PK_Discip" primary key (Sigla);
 --
 -- table: Aluno PK_Aluno; Type: CONSTRAINT; Owner: postgres
 --
 ALTER TABLE ONLY Aluno
-ADD CONSTRAINT "PK_Aluno" PRIMARY KEY (RA);
+add constraint "PK_Aluno" primary key (RA);
 --
 ------------------------------ RELACIONAMENTOS ------------------------------
 --
@@ -51,12 +57,16 @@ ADD CONSTRAINT "PK_Aluno" PRIMARY KEY (RA);
 -- Name: Matricula MatriculaRA; Type: FK CONSTRAINT; Owner: postgres
 --
 ALTER TABLE ONLY Matricula
-ADD CONSTRAINT "MatriculaRA" FOREIGN KEY (RA) REFERENCES Aluno(RA) ON UPDATE CASCADE ON DELETE CASCADE;
+add constraint "MatriculaRA" foreign key (RA) references Aluno(RA) on
+update cascade on
+	delete cascade;
 --
 -- Name: Matricula MatriculaSigla; Type: FK CONSTRAINT; Owner: postgres
 --
 ALTER TABLE ONLY Matricula
-ADD CONSTRAINT "MatriculaSigla" FOREIGN KEY (Sigla) REFERENCES Discip(Sigla) ON UPDATE CASCADE ON DELETE CASCADE;
+add constraint "MatriculaSigla" foreign key (Sigla) references Discip(Sigla) on
+update cascade on
+	delete cascade;
 ------
 -------------------- Funções Uteis ---------------------
 ------
@@ -75,13 +85,13 @@ end;
 $$language plpgsql;
 --select data()
 --Texto aleatorio
-Create or replace function texto(tamanho integer) returns text as $$
+create or replace function texto(tamanho integer) returns text as $$
 declare chars text [] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
 result text := '';
 i integer := 0;
 begin if tamanho < 0 then raise exception 'Tamanho dado nao pode ser menor que zero';
 end if;
-for i in 1..tamanho loop result := result || chars [1+random()*(array_length(chars, 1)-1)];
+for i in 1..tamanho loop result := result || chars [1 + random()*(array_length(chars, 1)-1)];
 end loop;
 return result;
 end;
@@ -92,8 +102,8 @@ $$ language plpgsql;
 ------
 do $$
 declare varEstado varchar [] = '{"PR", "SC", "RS", "SP"}';
-begin for i in 1..10 loop for j in 1..4 loop
-INSERT INTO Aluno (
+begin for i in 1..10000 loop for j in 1..4 loop
+insert into Aluno (
     Nome,
     RA,
     DataNasc,
@@ -104,7 +114,7 @@ INSERT INTO Aluno (
     Curso,
     periodo
   )
-VALUES(
+values(
     texto(20),
     numero(8),
     data(),
@@ -125,8 +135,8 @@ analyze Aluno;
 ------
 do $$
 declare varDepartamento varchar [] = '{"DAEELE", "DAINF", "DAMAT", "DALET"}';
-begin for i in 1..10 loop for j in 1..4 loop
-INSERT INTO Discip (
+begin for i in 1..10000 loop for j in 1..4 loop
+insert into Discip (
     Sigla,
     Nome,
     SiglaPreReq,
@@ -134,7 +144,7 @@ INSERT INTO Discip (
     Monitor,
     Depto
   )
-VALUES(
+values(
     texto(7),
     texto(20),
     texto(7),
@@ -150,20 +160,22 @@ analyze Discip;
 ------
 -------------- Popular Matricula
 ------
-DO $$
-DECLARE r record;
-DECLARE r2 record;
-BEGIN FOR r IN
-SELECT *
-FROM aluno LOOP RAISE NOTICE '%',
-  r;
-for r2 in
-select *
-from discip LOOP
-INSERT INTO Matricula
-VALUES (
-    r.RA,
-    r2.Sigla,
+do $$
+declare varRA int;
+declare varSigla char(20);
+begin for i in 1..1500 loop
+select ra into varRA
+from aluno
+order by random()
+limit 1;
+select Sigla into varSigla
+from Discip
+order by random()
+limit 1;
+insert into Matricula
+values (
+    varRA,
+    varSigla,
     numero(4)::char,
     numero(1)::char,
     numero(2),
@@ -172,63 +184,194 @@ VALUES (
     numero(2),
     numero(2),
     numero(2)
-  );
+  ) on conflict do nothing;
 end loop;
-END LOOP;
-END;
-$$;
-
+end;
+$$ language plpgsql;
+analyze Matricula;
 ---------------- questâo 2
-select  count(*) from aluno;
-CREATE UNIQUE INDEX IdxAlunoNNI ON Aluno (Nome, NomeMae, Idade);
+select count(*)
+from aluno;
+create unique index IdxAlunoNNI on Aluno (Nome, NomeMae, Idade);
 analyze;
 --- usando index
-explain select * from aluno  where nome = 'aaaa';
+explain
+select *
+from aluno
+where nome = 'aaaa';
 --
 --- não usando index
 -- quando usado a expressão do index, juntamente com or o index não acessado
-explain select * from aluno  where nome = 'aaaa' or NomeMae = 'bbb';
+explain
+select *
+from aluno
+where nome = 'aaaa'
+  or NomeMae = 'bbb';
 --
 ---------------- FIM questâo 2
 ---------------- questâo 3
---- a) Sequential Scan -> consulta em qualquer tabela sem index retorna esse plano
-explain select  * from aluno  where cidade = 'aa';
---- b) Bitmap Index Scan -> criado para estado por possuir 4 variações
+--  a Sequential Scan->consulta em qualquer tabela sem index retorna esse plano
+explain select *
+from aluno
+where cidade = 'aa';
+--  b Bitmap Index Scan->criado para estado por possuir 4 variações create extension btree_gin;
 create extension btree_gin;
 create index idxEstadoBitmap on aluno using gin (Estado);
 analyze aluno;
-explain select  * from aluno  where estado = 'SP';
-
---- c) Index Scan -> busca em chave primária ou diretamento em outro index
---- chave primária
-explain select  * from aluno  where ra = 185423;
+explain
+select *
+from aluno
+where estado = 'SP';
+--  c Index Scan->busca em chave primária ou diretamento em outro index --- chave primária
+explain analyze
+select *
+from aluno
+where ra = 185423;
 ---  em cima de um index criado
-CREATE INDEX IdxCidade ON Aluno (Cidade);
-explain select  * from aluno  where Cidade = 'cidade';
-
---- d) Index-Only Scan -> busca em chave cima do index projetando somento a a coluna do index
---  index chave primeira primária
-explain select ra from aluno where  ra = 123;
+create index IdxCidade on Aluno (Cidade);
+explain
+select *
+from aluno
+where Cidade = 'cidade';
+--  d Index - Only Scan->busca em chave cima do index projetando somento a a coluna do index --  index chave primeira primária
+explain analyze
+select ra
+from aluno
+where ra = 123;
 --- em cima do index criado anteriormente
-explain select  Cidade from aluno  where Cidade = 'cidade';
---- e) Multi-Index Scan -> (TODO) FALTA COMPLETAR
-explain select nome, ra from aluno  where Nome = 'nome' or ra = 123;
+explain analyze
+select Cidade
+from aluno
+where Cidade = 'cidade';
+--  e Multi - Index Scan
+explain analyze
+select nome,
+  ra
+from aluno
+where Nome = 'nome'
+  and ra = 123;
 ---------------- FIM questâo 3
 ---- Exercı́cio 4)  Faça consultas com junções entre as tabelas e mostre o desempenho criando-se ı́ndices para cada chave estrangeira.
-CREATE INDEX IdxFkSigla ON Matricula (Sigla);
-CREATE INDEX IdxFkRA ON Matricula (RA);
-
+create index IdxFkSigla on Matricula (Sigla);
+create index IdxFkRA on Matricula (RA);
 analyze;
-
 --------- com o uso do index da FK, consulta  muito mais mai rápida  ---------
-explain analyze select * from matricula m join aluno a on a.ra = m.ra where m.ra = 123;
-
---------- Sem o uso do index da FK, consulta demora até muito mais  ---------
-explain analyze select * from matricula m join aluno a on a.ra = m.ra where m.codturma = 123;
+explain analyze
+select *
+from matricula m
+  join aluno a on a.ra = m.ra
+where m.ra = 123;
+--Nested Loop  (cost=0.57..16.61 rows=1 width=168) (actual time=0.016..0.017 rows=0 loops=1)
+--  ->  Index Scan using idxfkra on matricula m  (cost=0.28..8.29 rows=1 width=46) (actual time=0.015..0.016 rows=0 loops=1)
+--        Index Cond: (ra = '123'::numeric)
+--  ->  Index Scan using "PK_Aluno" on aluno a  (cost=0.29..8.31 rows=1 width=122) (never executed)
+--        Index Cond: (ra = '123'::numeric)
+--Planning Time: 0.185 ms
+--Execution Time: 0.043 ms
+--------- Sem o uso do index da FK, consulta demora muito mais  ---------
+explain analyze
+select *
+from matricula m
+  join aluno a on a.ra = m.ra
+where m.codturma = 123;
+--Nested Loop  (cost=0.29..43.06 rows=1 width=168) (actual time=0.289..0.290 rows=0 loops=1)
+--  ->  Seq Scan on matricula m  (cost=0.00..34.75 rows=1 width=46) (actual time=0.289..0.289 rows=0 loops=1)
+--        Filter: (codturma = '123'::numeric)
+--        Rows Removed by Filter: 1500
+--  ->  Index Scan using "PK_Aluno" on aluno a  (cost=0.29..8.31 rows=1 width=122) (never executed)
+--        Index Cond: (ra = m.ra)
+--Planning Time: 0.399 ms
+--Execution Time: 0.313 ms
 -------------
 --- Exercı́cio 5) Utilize um ı́ndice bitmap para perı́odo e mostre-o em uso nas consultas.
 create index idxPeriodBitmap on aluno using gin (Periodo);
-explain analyze select  * from aluno  where periodo = 4;
-
-explain analyze select periodo from aluno  where periodo = 4 and curso = 'abc';
+explain analyze
+select *
+from aluno
+where periodo = 4;
+explain analyze
+select periodo
+from aluno
+where periodo = 4
+  and curso = 'abc';
 -------------
+--Exercı́cio 6) Compare na prática o custo de executar uma consulta com e sem um ı́ndice clusterizado
+--na tabela aluno. Ou seja, faça uma consulta sobre algum dado indexado, clusterize a tabela naquele
+--ı́ndice e refaça a consulta. Mostre os comandos e os resultados do explain analyze.
+explain analyze
+select *
+from aluno
+where nome = 'aaaa';
+--Index Scan using idxalunonni on aluno  (cost=0.42..8.44 rows=1 width=146) (actual time=0.016..0.016 rows=0 loops=1)
+--  Index Cond: ((nome)::text = 'aaaa'::text)
+--Planning Time: 0.370 ms
+--Execution Time: 0.036 ms
+alter table Aluno cluster on idxalunonni;
+cluster Aluno;
+analyze aluno;
+--
+explain analyze
+select *
+from aluno
+where nome = 'aaaa';
+-- Index Scan using idxalunonni on aluno  (cost=0.42..8.44 rows=1 width=146) (actual time=0.018..0.018 rows=0 loops=1)
+--  Index Cond: ((nome)::text = 'aaaa'::text)
+--Planning Time: 0.353 ms
+--Execution Time: 0.030 ms
+--
+--
+-------------
+--- Exercı́cio 7) Acrescente um campo adicional na tabela de Aluno, chamado de informacoesExtras, do
+--tipo JSON. Insira dados diferentes telefônicos e de times de futebol que o aluno torce para cada aluno
+--neste JSON. Crie ı́ndices para o JSON e mostre consultas que o utiliza (explain analyze). Exemplo:
+--retorne os alunos que torcem para o Internacional.
+--------- Adicionar coluna JSON
+alter table Aluno
+add informacoesExtras jsonb;
+--
+-------- update aluno com jsons
+do $$
+declare varRA int;
+declare varTimes varchar [] = '{"internacional", "são paulo", "grêmio", "palmeiras"}';
+begin for i in 1..250 loop for j in 1..4 loop
+select ra into varRA
+from aluno
+order by random()
+limit 1;
+update Aluno
+set informacoesExtras = (
+    (
+      '
+{
+"telefone" : ' || numero(9) || ',
+"time" : "' || varTimes [j] || '"
+}'
+    )::json
+  )
+where RA = varRA;
+end loop;
+end loop;
+end;
+$$ language plpgsql;
+analyze aluno;
+--
+-- irá fazer um seq scan
+explain analyze
+select informacoesextras
+from aluno
+where informacoesextras->>'time' = 'grêmio';
+create index idxTimeJson on Aluno using BTREE ((informacoesextras->>'time'));
+--
+-- irá utilizar o index idxTimeJson
+explain analyze
+select informacoesextras
+from aluno
+where informacoesextras->>'time' = 'grêmio';
+create index idxTimeInter on Aluno using BTREE ((informacoesextras->>'time'))
+where informacoesextras->>'time' = 'internacional';
+--
+-- irá utilizar o index idxTimeInter primeiramente e após o idxTimeJson em um bitmap heap scan
+explain analyze
+select informacoesextras
+from aluno
+where informacoesextras->>'time' = 'internacional';
--- banco-de-dados-2/prova-1/readme.md
+++ banco-de-dados-2/prova-1/readme.md
@@ -13,6 +13,9 @@ Para o desenvolvimento, foi utilizado docker para rodar o banco e postgres na ve
 docker run -d --name p1-banco-2 -e POSTGRES_PASSWORD=docker -p 5432:5432 postgres:13.4
 
 # Após pode ser conectado em alguma interface com o user "postgres", senha "docker", host "localhost", porta "5432"
+
+#  Para fazer dump da base com o docker 
+docker exec -i p1-banco-2  /bin/bash -c "PGPASSWORD=docker pg_dump --username postgres postgres" > ./dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 ```
 
 
